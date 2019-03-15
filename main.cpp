e0d1f92ff40ce79f7f5e8878b3da5c17b2287fbf
tonypnode <sgtp@me.com>
2019-03-14 23:01:32 -0400

    Refactor for functions
    Signed-off-by: tonypnode <sgtp@me.com>

--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 
+
 using namespace std;
 
-int main() {
+
+// Introduce the game
+void PrintIntro() {
 
    constexpr int WORD_LENGTH = 5;
 
-   cout << "Welcome to Bulls and Cows.\n\n Yea.. it's a game\n";
-   cout << "Guess the " << WORD_LENGTH << " letter isogram";
+   cout << "Welcome to Bulls and Cows.\n\n";
+   cout << "Guess the " << WORD_LENGTH << " Letter isogram\n";
+
+}
+
+string GetPlayerGuess() {
+   string Guess;
+
+   cout << "Enter Guess: \n";
+   std::getline(cin, Guess);
+
+   cout << "Your Guess was: " << Guess << endl;
+   return Guess;
+}
+
+int main() {
+
+   PrintIntro();
+
+   GetPlayerGuess();
 
    return 0;
 }
